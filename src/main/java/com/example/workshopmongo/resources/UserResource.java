e7c56af67da3f0d1d75af6336ae872270dc51195
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-02-09 19:05:41 -0300

    Update user with Put

--- src/main/java/com/example/workshopmongo/resources/UserResource.java
+++ src/main/java/com/example/workshopmongo/resources/UserResource.java
@@ -53,4 +53,13 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(value= "/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<UserDTO> update(@RequestBody UserDTO objDto, @PathVariable String id)
+	{
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
 }
--- src/main/java/com/example/workshopmongo/services/UserService.java
+++ src/main/java/com/example/workshopmongo/services/UserService.java
@@ -39,4 +39,18 @@ public class UserService {
 		findById(id);
 		repo.deleteById(id);
 	}
+	
+	public User update(User obj)
+	{
+		Optional<User> newUser = repo.findById(obj.getId());
+		User user = newUser.get();
+		updateData(user, obj);
+		return repo.save(user);
+	}
+
+	private void updateData(User user, User obj) {
+		user.setName(obj.getName());
+		user.setEmail(obj.getEmail());
+		
+	}
 }
