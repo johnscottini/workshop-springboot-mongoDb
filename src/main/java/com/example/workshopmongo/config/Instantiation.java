e4feeaf9f9c4cdbc86a89ef40f4914537df3103c
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-02-09 14:05:35 -0300

    Instantiate data operation

---
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -0,0 +1,26 @@
+package com.example.workshopmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.workshopmongo.domain.User;
+import com.example.workshopmongo.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+	}
+
+}
