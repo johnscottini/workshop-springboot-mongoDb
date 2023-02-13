928f234f582bcf66f35c71b4b5994f4cf868dfb2
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-02-13 18:25:21 -0300

    Post findById method

---
+++ src/main/java/com/example/workshopmongo/resources/PostResource.java
@@ -0,0 +1,28 @@
+package com.example.workshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.workshopmongo.domain.Post;
+
+import com.example.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value ="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+	
+	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id)
+	{
+		Post post = service.findById(id);
+		return ResponseEntity.ok().body(post);
+		
+	}
+}
---
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.example.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.workshopmongo.domain.Post;
+import com.example.workshopmongo.exception.ObjectNotFoundException;
+import com.example.workshopmongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id)
+	{
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
+	}
+}
