ccfcaa825ce6e6ce6d7dec4196ab801cf7abeea4
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-02-13 19:21:31 -0300

    Simple query using @Query

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongo.domain.Post;
@@ -10,5 +11,7 @@ import com.example.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> findByTitle(String text);
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
