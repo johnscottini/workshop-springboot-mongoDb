5382890ad9aae28f8723b639069ec1625226bc25
Jonathan Scottini <jonathanoliveirascottini8@gmail.com>
2023-02-13 21:02:03 -0300

    Query with multiple criteria

--- src/main/java/com/example/workshopmongo/services/PostService.java
+++ src/main/java/com/example/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +25,9 @@ public class PostService {
 	public List<Post> findByTitle (String text)
 	{
 		return repo.findByTitle(text);
+	}
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
